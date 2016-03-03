98300f6cfc7b9d40766cb9f2e7e21d1eff1b17db
svidya <svidya1725@gmail.com>
2016-03-03 13:39:34 +0000

    TapeEquilibrium class changes

--- Codility/Codility/TapeEquilibrium.cs
+++ Codility/Codility/TapeEquilibrium.cs
@@ -6,7 +6,32 @@ using System.Threading.Tasks;
 
 namespace Codility
 {
-    public class TapeEquilibrium
+    public static class TapeEquilibrium
     {
+        private const int maxLenght = 100000;
+        public static int GetMinimalDifferenceFromArray(int[] A)
+        {
+
+            int arrayLength = A.Length;
+            int[] partialSums = new int[maxLenght];
+
+            // calculate partial sums
+            for (int i = 0; i < arrayLength; i++)
+            {
+                partialSums[i] = i > 0 ? A[i] + partialSums[i - 1] : A[i];
+            }
+
+            int minimalValue = int.MaxValue;
+
+            // get minimal value
+            for (int i = 0; i < arrayLength - 1; i++)
+            {
+                int difference = Math.Abs(partialSums[i] - (partialSums[arrayLength - 1] - partialSums[i]));
+
+                minimalValue = difference < minimalValue ? difference : minimalValue;
+            }
+            return minimalValue;
+
+        }
     }
 }
--- Codility/CodilityTestProject/TapeEquillibriumTest.cs
+++ Codility/CodilityTestProject/TapeEquillibriumTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Codility;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CodilityTestProject
@@ -7,8 +8,13 @@ namespace CodilityTestProject
     public class TapeEquillibriumTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Get_Minimal_Difference_From_Array()
         {
+            int[] A = { 3,1,2,4,3};
+
+            int minimalDifference = TapeEquilibrium.GetMinimalDifferenceFromArray(A);
+
+            Assert.AreEqual(1, minimalDifference);
         }
     }
 }
